f2dc07371a5b69d57d19dec72ecf848f651a2d5b
Robert Zhao <rz87@cornell.edu>
2022-08-27 21:33:20 -0400

    Add examples for JSQLParser

--- M1/src/DBMS/Main.java
+++ M1/src/DBMS/Main.java
@@ -1,9 +1,15 @@
 package DBMS;
 
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws JSQLParserException {
+        Expression e= CCJSqlParserUtil.parseExpression("1");
         int a= 2 + 2;
         System.out.println(a);
         System.out.println("Hello World!");
+        System.out.println(e);
     }
 }
