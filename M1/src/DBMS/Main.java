f0b636e7aa226ac218d489770c06d70db913e987
Ben Kogan <brk57@cornell.edu>
2022-08-27 22:41:23 -0400

    Import jsqlparser example from canvas (#1)
    * import jsqlparser example from canvas
    
    * remove print

--- M1/src/DBMS/Main.java
+++ M1/src/DBMS/Main.java
@@ -1,15 +1,33 @@
 package DBMS;
 
-import net.sf.jsqlparser.JSQLParserException;
-import net.sf.jsqlparser.expression.Expression;
-import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import java.io.FileReader;
+import net.sf.jsqlparser.parser.CCJSqlParser;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.select.Select;
 
+/**
+ * Example class for getting started with JSQLParser. Reads SQL statements from
+ * a file and prints them to screen; then extracts SelectBody from each query
+ * and also prints it to screen.
+ * 
+ * @author Lucja Kot
+ */
 public class Main {
-    public static void main(String args[]) throws JSQLParserException {
-        Expression e= CCJSqlParserUtil.parseExpression("1");
-        int a= 2 + 2;
-        System.out.println(a);
-        System.out.println("Hello World!");
-        System.out.println(e);
-    }
+
+	private static final String queriesFile = "samples/input/queries.sql";
+
+	public static void main(String[] args) {
+		try {
+			CCJSqlParser parser = new CCJSqlParser(new FileReader(queriesFile));
+			Statement statement;
+			while ((statement = parser.Statement()) != null) {
+				System.out.println("Read statement: " + statement);
+				Select select = (Select) statement;
+				System.out.println("Select body is " + select.getSelectBody());
+			}
+		} catch (Exception e) {
+			System.err.println("Exception occurred during parsing");
+			e.printStackTrace();
+		}
+	}
 }
