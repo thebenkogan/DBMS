ad469af9de124463a25f4f7c24b05fcfdf2ce67b
Robert Zhao <rz87@cornell.edu>
2022-08-28 23:56:36 -0400

    Refactor Package System (#2)

---
+++ M1/src/test/DBMS/Tester.java
@@ -0,0 +1,14 @@
+package DBMS;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class Tester {
+
+    @Test
+    void test() {
+        assertEquals(4, 2 + 2);
+    }
+
+}
