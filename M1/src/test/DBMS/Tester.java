fdd1bdc4f45e3885a51b86984139d7224c7bb696
Robert Zhao <rz87@cornell.edu>
2022-08-29 19:43:53 -0400

    Add parameterized test example (#4)

--- M1/src/test/DBMS/Tester.java
+++ M1/src/test/DBMS/Tester.java
@@ -1,14 +1,23 @@
 package DBMS;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class Tester {
 
     @Test
     void test() {
         assertEquals(4, 2 + 2);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints= { 1, 2, 3, 4, 5 })
+    void isPositive(int number) {
+        assertTrue(number > 0);
     }
 
 }
