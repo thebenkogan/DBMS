31ad6ce020ddf43a24c5726e2dc7c4b12c5e7bf1
Ben Kogan <brk57@cornell.edu>
2022-09-10 15:49:03 -0400

    Simplify tuple (#20)
    * tuples use LinkedHashMap
    
    * . -> /

--- M1/src/main/DBMS/utils/Tuple.java
+++ M1/src/main/DBMS/utils/Tuple.java
@@ -1,18 +1,18 @@
 package DBMS.utils;
 
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 import net.sf.jsqlparser.schema.Column;
 
 public class Tuple {
-    /** Maps table name to another map mapping column name to field value. */
-    private Map<String, Map<String, Integer>> items;
+    /** Maps table.column key to value in row. Insertion order represents column ordering. */
+    private LinkedHashMap<String, Integer> row;
 
-    /** Specifies the order of the columns, where columnOrder[i] is [tableName, columnName] */
-    private String[][] columnOrder;
+    /** Constructs a key to look up the corresponding value in the row */
+    private String key(String tableName, String columnName) {
+        return tableName + "/" + columnName;
+    }
 
     /** Creates a new tuple for the items in columns. Requires columns and items are of the same
      * size.
@@ -21,27 +21,20 @@ public class Tuple {
      * @param columns   the name of each column in the table
      * @param data      the row of data in the table */
     public Tuple(String tableName, List<String> columns, List<Integer> data) {
-        String[][] columnOrder= new String[columns.size()][2];
-        Map<String, Map<String, Integer>> items= new HashMap<>();
-        items.put(tableName, new HashMap<>());
+        row= new LinkedHashMap<>();
         for (int i= 0; i < data.size(); i++ ) {
-            items.get(tableName).put(columns.get(i), data.get(i));
-            columnOrder[i][0]= tableName;
-            columnOrder[i][1]= columns.get(i);
+            row.put(key(tableName, columns.get(i)), data.get(i));
         }
-        this.items= items;
-        this.columnOrder= columnOrder;
     }
 
-    private Tuple(Map<String, Map<String, Integer>> items, String[][] columnOrder) {
-        this.items= items;
-        this.columnOrder= columnOrder;
+    private Tuple(LinkedHashMap<String, Integer> row) {
+        this.row= row;
     }
 
     /** @param columnName name of table column
      * @return value in the column */
     public int get(String tableName, String columnName) {
-        return items.get(tableName).get(columnName);
+        return row.get(key(tableName, columnName));
     }
 
     /** Projects this tuple to those in columns. Requires: columns is a subset of the columns in
@@ -50,26 +43,18 @@ public class Tuple {
      * @param columns projected columns */
     public void project(List<Column> columns) {
         Integer[] data= columns.stream()
-            .map(c -> items.get(c.getTable().getName()).get(c.getColumnName()))
+            .map(c -> row.get(key(c.getTable().getName(), c.getColumnName())))
             .toArray(Integer[]::new);
-        items.clear();
-        columnOrder= new String[columns.size()][2];
+        row.clear();
         for (int i= 0; i < data.length; i++ ) {
-            String tableName= columns.get(i).getTable().getName();
-            items.putIfAbsent(tableName, new HashMap<>());
-            items.get(tableName).put(columns.get(i).getColumnName(), data[i]);
-            columnOrder[i][0]= tableName;
-            columnOrder[i][1]= columns.get(i).getColumnName();
+            Column col= columns.get(i);
+            row.put(key(col.getTable().getName(), col.getColumnName()), data[i]);
         }
     }
 
     @Override
     public String toString() {
-        int[] data= new int[columnOrder.length];
-        for (int i= 0; i < columnOrder.length; i++ ) {
-            data[i]= this.get(columnOrder[i][0], columnOrder[i][1]);
-        }
-        return Arrays.toString(data).replaceAll("\\s|\\[|\\]", "");
+        return row.values().toString().replaceAll("\\s|\\[|\\]", "");
     }
 
     /** Merges left and right into a new Tuple. Merged columnOrder is the concatenation of
@@ -78,21 +63,9 @@ public class Tuple {
      * @param left  left tuple
      * @param right right tuple */
     public static Tuple mergeTuples(Tuple left, Tuple right) {
-        int mergedLength= left.columnOrder.length + right.columnOrder.length;
-        String[][] mergedOrder= new String[mergedLength][2];
-        int[] mergedData= new int[mergedLength];
-        Map<String, Map<String, Integer>> mergedItems= new HashMap<>();
-
-        for (int i= 0; i < mergedLength; i++ ) {
-            Tuple refTuple= i < left.columnOrder.length ? left : right;
-            String[] entry= i < left.columnOrder.length ? left.columnOrder[i] :
-                right.columnOrder[i - left.columnOrder.length];
-            mergedOrder[i]= entry;
-            mergedData[i]= refTuple.get(entry[0], entry[1]);
-            mergedItems.putIfAbsent(entry[0], new HashMap<>());
-            mergedItems.get(entry[0]).put(entry[1], mergedData[i]);
-        }
-
-        return new Tuple(mergedItems, mergedOrder);
+        LinkedHashMap<String, Integer> row= new LinkedHashMap<>();
+        left.row.forEach((key, value) -> row.put(key, value));
+        right.row.forEach((key, value) -> row.put(key, value));
+        return new Tuple(row);
     }
 }
