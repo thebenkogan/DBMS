c4047a99800ea0c613e36fda5a5dc7bf65765806
Robert Zhao <rz87@cornell.edu>
2022-11-01 20:45:37 -0400

    Fix JUnit4 in New Tests (#105)

--- src/test/java/com/dbms/index/TreeDeserializerTest.java
+++ src/test/java/com/dbms/index/TreeDeserializerTest.java
@@ -1,7 +1,7 @@
 package com.dbms.index;
 
-import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import com.dbms.utils.Catalog;
 import com.dbms.utils.ColumnName;
